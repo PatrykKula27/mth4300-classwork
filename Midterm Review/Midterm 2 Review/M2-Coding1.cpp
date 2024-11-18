11f496b84afe2a7a982e287e46c2750c2ee88c3a
PatrykKula27 <patrykkulabusiness@gmail.com>
2024-11-18 16:52:40 -0500

    Midterm 2 Notes - Complete

--- Midterm Review/Midterm 2 Review/M2-Coding1.cpp
+++ Midterm Review/Midterm 2 Review/M2-Coding1.cpp
@@ -5,21 +5,21 @@
 
 bool LinkedList::push_back(int val)
 {
-    newNode = new Node;
-    newNode->data = val;
-    newNode->next = nullptr;
+    newNode = new Node; // Create a new node
+    newNode->data = val; // Assigns the value to the node's data
+    newNode->next = nullptr; // Points next to nullptr
 
-    if (head == nullptr)
+    if (head == nullptr) // Assigns newNode to the head because there are no nodes in the linked list.
     {
         head = newNode;
         return 1;
     }
 
-    Node* curr = head;
-    while (curr->next)
+    Node* curr = head; // Creates the node pointer current and points it to the beginning of the linked list.
+    while (curr->next) // While the current pointer still points to an existent node that comes after it.
     {
-        curr = curr->next;
-    }
-    curr->next = newNode;
+        curr = curr->next; // Traverses through the linked list.
+    } // The while loop terminates once curr->next points to nullptr.
+    curr->next = newNode; // Replaces curr's nullptr with the new node, which includes the node, it's value, and a pointer to next (which is nullptr)
     return true;
 }
